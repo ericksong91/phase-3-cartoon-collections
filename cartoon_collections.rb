37796ab1d72730da16c331c744059376e1373e84
Eric Song <ericksong91@gmail.com>
2023-02-02 19:20:00 -0500

    Done with assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+
+ dwarves.map.with_index(1) {|dwarf, index|
+    pp "#{index}. #{dwarf}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+
+  new_calls = calls.map {|call| 
+    temp = call.capitalize()
+    temp << "!"
+  }
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+
+  #check each string in array length 
+
+  calls.map {|call|
+    if call.length < 4
+      return true
+    else
+      return false
+    end
+  }
+
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  new_food = food.select do |f|
+    cheese_types.any? {|cheese| f == cheese}
+  end
+
+  new_food[0]
 end
